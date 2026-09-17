34923e8880011e6234e025fb74fe05126a8bdacc
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-17 11:31:26 +0700

    refactor(api): route DashboardController through Application contracts

--- FVN_REGISTER.API/Controllers/DashboardController.cs
+++ FVN_REGISTER.API/Controllers/DashboardController.cs
@@ -1,11 +1,10 @@
-﻿using AutoMapper;
-using FVN_REGISTER.Contract.Interfaces.Repositores;
-using FVN_REGISTER.Contract.Interfaces.Statics;
-using FVN_REGISTER.Contract.Interfaces.Users;
+using AutoMapper;
+using FVN_REGISTER.Application.Interfaces.Statics;
+using FVN_REGISTER.Application.Interfaces.Users;
+using FVN_REGISTER.Contract.Dtos.Authentication;
 using FVN_REGISTER.Contract.ViewModels;
 using FVN_REGISTER.Core.Configurations;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
