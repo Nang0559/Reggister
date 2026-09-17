b7245b9948a0b1dea40a80e14e953f1776d11579
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-17 17:23:48 +0700

    fix: import RequestModule extensions in history base

--- FVN_REGISTER.Infrastructure/Services/Histories/BaseHistoryHandler.cs
+++ FVN_REGISTER.Infrastructure/Services/Histories/BaseHistoryHandler.cs
@@ -5,6 +5,7 @@ using FVN_REGISTER.Contract.Dtos.Histories;
 using FVN_REGISTER.Contract.Utils;
 using FVN_REGISTER.Core.Entities;
 using FVN_REGISTER.Core.Enums;
+using FVN_REGISTER.Core.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace FVN_REGISTER.Infrastructure.Services.Histories;
