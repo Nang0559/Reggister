c28e39c4191bd947a4b38f52fea30fc2dbf5524b
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-17 15:28:08 +0700

    refactor(contract): move PaginationResult into Contract

---
+++ FVN_REGISTER.Models/Utils/PaginationResult.cs
@@ -0,0 +1,43 @@
+namespace FVN_REGISTER.Contract.Utils;
+
+public class PaginationResult<T>
+{
+    public List<T> Data { get; set; } = new();
+
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+
+    public int TotalPages => PageSize <= 0
+        ? 0
+        : (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+    public bool HasNext => Page < TotalPages;
+    public bool HasPrevious => Page > 1;
+
+    public string? SortBy { get; set; }
+    public string? SortDirection { get; set; }
+
+    public string? ErrorMessage { get; set; }
+    public bool IsSuccess => string.IsNullOrWhiteSpace(ErrorMessage);
+
+    public PaginationResult() { }
+
+    public PaginationResult(List<T> data, int totalCount, int page, int pageSize)
+    {
+        Data = data;
+        TotalCount = totalCount;
+        Page = page;
+        PageSize = pageSize;
+    }
+
+    public static PaginationResult<T> Fail(string errorMessage)
+        => new()
+        {
+            ErrorMessage = errorMessage,
+            Data = new(),
+            TotalCount = 0,
+            Page = 1,
+            PageSize = 10
+        };
+}
