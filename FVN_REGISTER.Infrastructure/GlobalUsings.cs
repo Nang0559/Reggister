5a47792e1b50c18e8a69aacc181b2b3485fd4699
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-17 15:51:35 +0700

    add canonical Infrastructure global usings

---
+++ FVN_REGISTER.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,17 @@
+global using FVN_REGISTER.Infrastructure.Models.Data;
+
+global using FVN_REGISTER.Core.Entities.Approvers;
+global using FVN_REGISTER.Core.Entities.Common;
+global using FVN_REGISTER.Core.Entities.HR;
+global using FVN_REGISTER.Core.Entities.Leaves;
+global using FVN_REGISTER.Core.Entities.OT;
+global using FVN_REGISTER.Core.Entities.Security;
+global using FVN_REGISTER.Core.Entities.Trips;
+global using FVN_REGISTER.Core.Enums;
+
+global using FVN_REGISTER.Application.Configuration;
+global using FVN_REGISTER.Application.Interfaces.Approvals;
+global using FVN_REGISTER.Application.Interfaces.Histories;
+global using FVN_REGISTER.Application.Models.Subjects;
+
+global using Microsoft.Extensions.DependencyInjection;
