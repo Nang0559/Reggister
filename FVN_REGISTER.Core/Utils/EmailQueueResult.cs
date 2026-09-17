69c65f390077cecaa7235a7c8be7a8a46457c5f6
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-17 13:44:35 +0700

    refactor(core): move EmailQueueResult into Core

---
+++ FVN_REGISTER.Core/Utils/EmailQueueResult.cs
@@ -0,0 +1,24 @@
+namespace FVN_REGISTER.Core.Utils
+{
+    public class EmailQueueResult
+    {
+        public bool IsSuccess { get; set; }
+        public bool IsTemplateNotFound { get; set; }
+        public string? Message { get; set; }
+
+        public static EmailQueueResult Ok()
+            => new() { IsSuccess = true };
+
+        public static EmailQueueResult TemplateNotFound(string templateCode)
+            => new()
+            {
+                IsSuccess = false,
+                IsTemplateNotFound = true,
+                Message = $"Template email '{templateCode}' chưa được khai báo. " +
+                          $"Đơn đã được tạo nhưng email thông báo chưa được gửi."
+            };
+
+        public static EmailQueueResult Fail(string message)
+            => new() { IsSuccess = false, Message = message };
+    }
+}
