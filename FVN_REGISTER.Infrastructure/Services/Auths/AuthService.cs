165703bc35c0b67bd082b81343d77162f0a22b24
Nang Do Van <pc10@fcc-vn.com.vn>
2026-09-18 13:14:17 +0700

    ok

--- FVN_REGISTER.Infrastructure/Services/Auths/AuthService.cs
+++ FVN_REGISTER.Infrastructure/Services/Auths/AuthService.cs
@@ -1,14 +1,13 @@
 ﻿
-using FVN_REGISTER.Application.Configuration;
+
 using FVN_REGISTER.Application.Interfaces.Auths;
-using FVN_REGISTER.Application.Logging;
+
 using FVN_REGISTER.Application.Services.Common;
 using FVN_REGISTER.Contract.Dtos.Authentication;
 using FVN_REGISTER.Core.Constants;
-using FVN_REGISTER.Core.Entities.HR;
-using FVN_REGISTER.Core.Entities.Security;
+
 using FVN_REGISTER.Core.Repositories;
-using FVN_REGISTER.Core.Utils;
+
 using FVN_REGISTER.Infrastructure.Hubs;
 using FVN_REGISTER.Infrastructure.Utils;
 using Microsoft.AspNetCore.SignalR;
@@ -17,7 +16,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
