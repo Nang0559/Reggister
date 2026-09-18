080897f1e450962beea74802cddfdc1cee84cae8
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-18 19:07:12 +0700

    feat(hrm): register role rule service

--- FVN_REGISTER.API/Program.cs
+++ FVN_REGISTER.API/Program.cs
@@ -194,6 +194,7 @@ builder.Services.AddScoped<IHrmStagingImporterResolver, HrmStagingImporterResolv
 builder.Services.AddScoped<IHrmSyncJobResolver, HrmSyncJobResolver>();
 builder.Services.AddScoped<IHrmSyncReviewQueryService, HrmSyncReviewQueryService>();
 builder.Services.AddScoped<IHrmSyncService, HrmSyncService>();
+builder.Services.AddScoped<IHrmUserRoleRuleService, HrmUserRoleRuleService>();
 
 builder.Services.AddAuthentication(options =>
 {
