cb30ae5d936c7e4dbb041de9060eca723fb0220a
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-18 12:02:18 +0700

    Synchronize SQL with current EF model and documentation

--- SQL/04_Constraints.sql
+++ SQL/04_Constraints.sql
@@ -1,25 +1,21 @@
 USE [FVN_REGISTER];
 GO
-IF OBJECT_ID(N'auth.UserRole','U') IS NOT NULL
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_UserRole_User') ALTER TABLE auth.UserRole ADD CONSTRAINT FK_UserRole_User FOREIGN KEY(UserId) REFERENCES auth.UserAccount(Id);
-    IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_UserRole_Role') ALTER TABLE auth.UserRole ADD CONSTRAINT FK_UserRole_Role FOREIGN KEY(RoleId) REFERENCES auth.Role(Id);
-END
-IF OBJECT_ID(N'auth.RolePermission','U') IS NOT NULL
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_RolePermission_Role') ALTER TABLE auth.RolePermission ADD CONSTRAINT FK_RolePermission_Role FOREIGN KEY(RoleId) REFERENCES auth.Role(Id);
-    IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_RolePermission_Permission') ALTER TABLE auth.RolePermission ADD CONSTRAINT FK_RolePermission_Permission FOREIGN KEY(PermissionId) REFERENCES auth.Permission(Id);
-END
-IF OBJECT_ID(N'hr.Department','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_Department_Parent') ALTER TABLE hr.Department ADD CONSTRAINT FK_Department_Parent FOREIGN KEY(ParentId) REFERENCES hr.Department(Id);
-IF OBJECT_ID(N'hr.Employee','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_Employee_Department') ALTER TABLE hr.Employee ADD CONSTRAINT FK_Employee_Department FOREIGN KEY(DepartmentId) REFERENCES hr.Department(Id);
-IF OBJECT_ID(N'config.Approver','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_Approver_Level') ALTER TABLE config.Approver ADD CONSTRAINT FK_Approver_Level FOREIGN KEY(ApprovalLevelId) REFERENCES config.ApprovalLevel(Id);
-IF OBJECT_ID(N'leave.LeaveRequest','U') IS NOT NULL
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_LeaveRequest_LeaveType') ALTER TABLE leave.LeaveRequest ADD CONSTRAINT FK_LeaveRequest_LeaveType FOREIGN KEY(LeaveTypeId) REFERENCES config.LeaveType(Id);
-    IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_LeaveRequest_WorkYear') ALTER TABLE leave.LeaveRequest ADD CONSTRAINT FK_LeaveRequest_WorkYear FOREIGN KEY(WorkYearId) REFERENCES config.WorkYear(Id);
-END
-IF OBJECT_ID(N'leave.LeaveAttachment','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_LeaveAttachment_Request') ALTER TABLE leave.LeaveAttachment ADD CONSTRAINT FK_LeaveAttachment_Request FOREIGN KEY(LeaveRequestId) REFERENCES leave.LeaveRequest(Id) ON DELETE CASCADE;
-IF OBJECT_ID(N'leave.Approval','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_LeaveApproval_Request') ALTER TABLE leave.Approval ADD CONSTRAINT FK_LeaveApproval_Request FOREIGN KEY(LeaveRequestId) REFERENCES leave.LeaveRequest(Id) ON DELETE CASCADE;
-IF OBJECT_ID(N'ot.Approval','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_OTApproval_Request') ALTER TABLE ot.Approval ADD CONSTRAINT FK_OTApproval_Request FOREIGN KEY(OTRequestId) REFERENCES ot.OTRequest(Id) ON DELETE CASCADE;
-IF OBJECT_ID(N'trip.Approval','U') IS NOT NULL AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name=N'FK_TripApproval_Request') ALTER TABLE trip.Approval ADD CONSTRAINT FK_TripApproval_Request FOREIGN KEY(TripRequestId) REFERENCES trip.TripRequest(Id) ON DELETE CASCADE;
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03Permissions_Code' AND object_id=OBJECT_ID(N'dbo.F03Permissions')) CREATE UNIQUE INDEX IX_F03Permissions_Code ON dbo.F03Permissions(PermissionCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03Functions_Code' AND object_id=OBJECT_ID(N'dbo.F03Functions')) CREATE UNIQUE INDEX IX_F03Functions_Code ON dbo.F03Functions(FunctionCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03Users_EmployeeCode' AND object_id=OBJECT_ID(N'dbo.F03Users')) CREATE UNIQUE INDEX IX_F03Users_EmployeeCode ON dbo.F03Users(EmployeeCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03Departments_Code' AND object_id=OBJECT_ID(N'dbo.F03Departments')) CREATE UNIQUE INDEX IX_F03Departments_Code ON dbo.F03Departments(DeptCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03Positions_Code' AND object_id=OBJECT_ID(N'dbo.F03Positions')) CREATE UNIQUE INDEX IX_F03Positions_Code ON dbo.F03Positions(PositionCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03Genders_Code' AND object_id=OBJECT_ID(N'dbo.F03Genders')) CREATE UNIQUE INDEX IX_F03Genders_Code ON dbo.F03Genders(GenderCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03LeaveType_Code' AND object_id=OBJECT_ID(N'dbo.F03LeaveType')) CREATE UNIQUE INDEX IX_F03LeaveType_Code ON dbo.F03LeaveType(LeaveTypeCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03OTTypes_Code' AND object_id=OBJECT_ID(N'dbo.F03OTTypes')) CREATE UNIQUE INDEX IX_F03OTTypes_Code ON dbo.F03OTTypes(OTTypeCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03OTCodes_Code' AND object_id=OBJECT_ID(N'dbo.F03OTCodes')) CREATE UNIQUE INDEX IX_F03OTCodes_Code ON dbo.F03OTCodes(ReasonCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03OTRequests_Code' AND object_id=OBJECT_ID(N'dbo.F03OTRequests')) CREATE UNIQUE INDEX IX_F03OTRequests_Code ON dbo.F03OTRequests(OTCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03TripRequests_Code' AND object_id=OBJECT_ID(N'dbo.F03TripRequests')) CREATE UNIQUE INDEX IX_F03TripRequests_Code ON dbo.F03TripRequests(TripCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03EquipmentAssets_Code' AND object_id=OBJECT_ID(N'dbo.F03EquipmentAssets')) CREATE UNIQUE INDEX IX_F03EquipmentAssets_Code ON dbo.F03EquipmentAssets(EquipmentCode);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03EquipmentAssets_QrToken' AND object_id=OBJECT_ID(N'dbo.F03EquipmentAssets')) CREATE UNIQUE INDEX IX_F03EquipmentAssets_QrToken ON dbo.F03EquipmentAssets(QrToken);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03EquipmentRepair_Request' AND object_id=OBJECT_ID(N'dbo.F03EquipmentRepairHistory')) CREATE UNIQUE INDEX IX_F03EquipmentRepair_Request ON dbo.F03EquipmentRepairHistory(RequestId);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03EmailProfiles_Code' AND object_id=OBJECT_ID(N'dbo.F03EmailProfiles')) CREATE UNIQUE INDEX IX_F03EmailProfiles_Code ON dbo.F03EmailProfiles(Code);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03EmailTemplates_Code' AND object_id=OBJECT_ID(N'dbo.F03EmailTemplates')) CREATE UNIQUE INDEX IX_F03EmailTemplates_Code ON dbo.F03EmailTemplates(Code);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03BusinessRules_Module_Code' AND object_id=OBJECT_ID(N'dbo.F03BusinessRules')) CREATE UNIQUE INDEX IX_F03BusinessRules_Module_Code ON dbo.F03BusinessRules(Module,Code);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name=N'IX_F03LeaveBalance_Employee_Year' AND object_id=OBJECT_ID(N'dbo.F03LeaveBalances')) CREATE UNIQUE INDEX IX_F03LeaveBalance_Employee_Year ON dbo.F03LeaveBalances(EmployeeCode,WorkYear);
 GO
