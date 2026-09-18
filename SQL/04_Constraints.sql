cbf2d40d1460b0183759fb20a63a815f98340f04
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-18 13:51:12 +0700

    Align security SQL index names with EF configuration

--- SQL/04_Constraints.sql
+++ SQL/04_Constraints.sql
@@ -1,9 +1,9 @@
 USE [FVN_REGISTER];
 GO
 /* EF business-key indexes */
-IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_F03Permissions_Code' AND object_id=OBJECT_ID('dbo.F03Permissions')) CREATE UNIQUE INDEX IX_F03Permissions_Code ON dbo.F03Permissions(PermissionCode);
-IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_F03Functions_Code' AND object_id=OBJECT_ID('dbo.F03Functions')) CREATE UNIQUE INDEX IX_F03Functions_Code ON dbo.F03Functions(FunctionCode);
-IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_F03Users_EmployeeCode' AND object_id=OBJECT_ID('dbo.F03Users')) CREATE UNIQUE INDEX IX_F03Users_EmployeeCode ON dbo.F03Users(EmployeeCode);
+IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_Permission_Code' AND object_id=OBJECT_ID('dbo.F03Permissions')) CREATE UNIQUE INDEX IX_F03Permissions_Code ON dbo.F03Permissions(PermissionCode);
+IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_Function_Code' AND object_id=OBJECT_ID('dbo.F03Functions')) CREATE UNIQUE INDEX IX_F03Functions_Code ON dbo.F03Functions(FunctionCode);
+IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_User_EmployeeCode' AND object_id=OBJECT_ID('dbo.F03Users')) CREATE UNIQUE INDEX IX_F03Users_EmployeeCode ON dbo.F03Users(EmployeeCode);
 IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_F03Departments_Code' AND object_id=OBJECT_ID('dbo.F03Departments')) CREATE UNIQUE INDEX IX_F03Departments_Code ON dbo.F03Departments(DeptCode);
 IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_F03Positions_Code' AND object_id=OBJECT_ID('dbo.F03Positions')) CREATE UNIQUE INDEX IX_F03Positions_Code ON dbo.F03Positions(PositionCode);
 IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_F03Genders_Code' AND object_id=OBJECT_ID('dbo.F03Genders')) CREATE UNIQUE INDEX IX_F03Genders_Code ON dbo.F03Genders(GenderCode);
