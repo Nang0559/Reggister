ba9793b2e43e1b22b3d30e3654e0367c6c830209
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-18 23:02:20 +0700

    feat(security): add authorization scope constants

---
+++ FVN_REGISTER.Core/Constants/AuthorizationScopeCodes.cs
@@ -0,0 +1,10 @@
+namespace FVN_REGISTER.Core.Constants;
+
+public static class AuthorizationScopeCodes
+{
+    public const string None = "None";
+    public const string Own = "Own";
+    public const string Employee = "Employee";
+    public const string Department = "Department";
+    public const string All = "All";
+}
