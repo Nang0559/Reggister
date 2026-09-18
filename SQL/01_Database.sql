3c852b3d26110aa5c6874f39b391660854a68201
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-18 12:01:38 +0700

    Replace database bootstrap with executable SQL Server entry point

--- SQL/01_Database.sql
+++ SQL/01_Database.sql
@@ -1,8 +1,16 @@
-/* FVN_REGISTER - Database bootstrap */
+/*
+FVN_REGISTER - SQL Server deployment entry point.
+Run this file first in SSMS/sqlcmd.
+*/
 IF DB_ID(N'FVN_REGISTER') IS NULL
 BEGIN
     CREATE DATABASE [FVN_REGISTER];
 END
 GO
+
 USE [FVN_REGISTER];
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = N'dbo')
+    EXEC(N'CREATE SCHEMA [dbo]');
 GO
