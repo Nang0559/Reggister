361cb399aa080b0143a959fdc37475c3d4ae30ab
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-18 12:03:51 +0700

    Add SQL functions procedures audit permissions and verification aligned to model

--- SQL/99_Verify.sql
+++ SQL/99_Verify.sql
@@ -1,14 +1,10 @@
 USE [FVN_REGISTER];
 GO
-SELECT s.name AS [SchemaName],o.name AS [ObjectName],o.type_desc AS [Type]
-FROM sys.objects o JOIN sys.schemas s ON s.schema_id=o.schema_id
-WHERE o.is_ms_shipped=0
-ORDER BY s.name,o.type_desc,o.name;
-
-SELECT fk.name,OBJECT_SCHEMA_NAME(fk.parent_object_id) ParentSchema,
-       OBJECT_NAME(fk.parent_object_id) ParentTable,
-       OBJECT_SCHEMA_NAME(fk.referenced_object_id) RefSchema,
-       OBJECT_NAME(fk.referenced_object_id) RefTable
-FROM sys.foreign_keys fk
-ORDER BY fk.name;
+SET NOCOUNT ON;
+SELECT DB_NAME() AS DatabaseName;
+SELECT name,type_desc FROM sys.tables WHERE name LIKE N'F03%' OR name LIKE N'Approval%' OR name LIKE N'Hrm%' ORDER BY name;
+SELECT TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME,DATA_TYPE,CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=N'dbo' AND (TABLE_NAME LIKE N'F03%' OR TABLE_NAME LIKE N'Approval%' OR TABLE_NAME LIKE N'Hrm%') ORDER BY TABLE_NAME,ORDINAL_POSITION;
+SELECT name,type_desc FROM sys.views WHERE name IN(N'vEmployeeApprover',N'vF03Employee',N'vF03leaveType',N'vF03LeaveRequest',N'vF03LeaveRequestDetail',N'vF03LeaveBalance',N'vF03OTRequest',N'vF03OTRequestDetail',N'vF03OTSummary',N'vF03Users',N'vw_CurrentlyPresentEmployees',N'vF03EmployeeAttendance',N'VwShiftCheckInOut');
+SELECT name,type_desc FROM sys.procedures WHERE name IN(N'usp_GetPendingApproval',N'usp_DecideApproval',N'usp_SyncAttendanceStaging',N'usp_SyncOTActualHours',N'usp_DequeueEmail',N'usp_ProcessApprovalEscalation',N'usp_WriteAuditLog',N'usp_WriteUserLog');
+SELECT fk.name,OBJECT_NAME(fk.parent_object_id) AS ParentTable,OBJECT_NAME(fk.referenced_object_id) AS ReferencedTable FROM sys.foreign_keys fk ORDER BY ParentTable,fk.name;
 GO
