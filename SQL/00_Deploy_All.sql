eb486e86185e75e4640d66db10c561a953369c16
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-19 19:44:50 +0700

    build(sql): deploy HRM user and approval provisioning stage

--- SQL/00_Deploy_All.sql
+++ SQL/00_Deploy_All.sql
@@ -52,6 +52,7 @@ IMPORTANT:
 :r 10_Triggers.sql
 :r 11_Automation.sql
 :r 12_Verify.sql
+:r 13_Hrm_User_Approval_Provisioning.sql
 :r 13_HrmShiftMaster.sql
 :r 14_SecurityAuthorization.sql
 :r 15_PublicInformation.sql
