2f37f08186d4ba27578fe058947578d8edd14156
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-20 00:01:53 +0700

    HRM attendance: return isolated calculation result to FVN orchestrator

--- SQL/22_02_HrmCompatibleTimeKeepingForStaff.sql
+++ SQL/22_02_HrmCompatibleTimeKeepingForStaff.sql
@@ -2840,5 +2840,20 @@ CREATE OR ALTER PROCEDURE dbo.usp_HrmCompatibleTimeKeepingForStaff
 			,BCTGUuDaiD=@TGUuDaiD
 			--,BCDaXacNhanLamThem=1
 		WHERE (BCMaNV=@StaffID  AND (BCNgay= @D))
+
+	-- Return only the HRM result columns consumed by the FVN orchestration.
+	SELECT
+		BCNgay,BCMaNV,BCMaBP,BCMaCV,BCMaCa,
+		BCCuaDen,BCTGDen,BCCuaVe,BCTGVe,BCCuaRa,BCTGRa,BCCuaVao,BCTGVao,
+		BCTGLamNgay,BCTGLamToi,BCTGQuaGioNgay,BCTGQuaGioToi,BCTGQuaGioNgayTC,BCTGQuaGioToiTC,
+		BCTGThemNgay,BCTGThemToi,BCTGRaNgoaiNgay,BCTGRaNgoaiToi,BCTGDiMuonNgay,BCTGDiMuonToi,
+		BCTGVeSomNgay,BCTGVeSomToi,BCTGQuyDinh,BCGhiChu,BCLoai,BCLoaiLamThem,BCTinhLamThem,
+		BCNghiBuChoNgay,BCNghiPhep,BCNghiH100,BCNghiH70,BCNghiKL,BCNghiBH100,BCNghiBH70,
+		BCNghiCongTac,BCNghiBu,BCNghiKhac,BCLoaiNgayNghi,BCLydonghi,BCTGNghi,
+		BCTGDKNTheogio,BCLoaiDKN,BCDangKyLTN,BCDangKyLTD,BCTGUuDaiN,BCTGUuDaiD,
+		BCNgayLe,BCNgayLeNV,BCDaXacNhanLamThem,DLocked
+	FROM #tblBaoCao
+	WHERE BCMaNV=@StaffID AND BCNgay=@D;
+
 END
 
