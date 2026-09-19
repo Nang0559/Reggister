035e000419d711e2c04b5ede49692da6f5278430
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-19 21:45:35 +0700

    Fix approval policy position service contract

--- Application/Interfaces/Approvals/IApprovalPolicyService.cs
+++ Application/Interfaces/Approvals/IApprovalPolicyService.cs
@@ -7,6 +7,7 @@ namespace FVN_REGISTER.Application.Interfaces.Approvals;
 public interface IApprovalPolicyService
 {
     Task<ServiceResult<List<ApprovalPolicyDto>>> GetAllAsync(CancellationToken ct = default);
+    Task<ServiceResult<List<ApprovalPolicyPositionDto>>> GetPositionsAsync(CancellationToken ct = default);
     Task<ServiceResult<ApprovalPolicyDto>> CreateAsync(ApprovalPolicyRequest request, int actorUserId, CancellationToken ct = default);
     Task<ServiceResult<ApprovalPolicyDto>> UpdateAsync(int id, ApprovalPolicyRequest request, int actorUserId, CancellationToken ct = default);
     Task<ServiceResult<object>> DeleteAsync(int id, int actorUserId, CancellationToken ct = default);
