06f13d136e342294e1c786b67c6cda20df543546
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-19 10:04:25 +0700

    Normalize security entities to BaseAuditEntity Id

--- FVN_REGISTER.Core/Entities/Security/F03RoleFunction.cs
+++ FVN_REGISTER.Core/Entities/Security/F03RoleFunction.cs
@@ -1,12 +1,17 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using FVN_REGISTER.Core.Entities;
 
 namespace FVN_REGISTER.Core.Entities.Security;
 
 [Table("F03RoleFunctions")]
-public sealed class F03RoleFunction:BaseAuditEntity
+public sealed class F03RoleFunction : BaseAuditEntity
 {
-    
+    public int IdRole { get; set; }
     public int IdFunction { get; set; }
-    public  F03Role Role { get; set; } = null!;
-    public  F03Function Function { get; set; } = null!;
+
+    [ForeignKey(nameof(IdRole))]
+    public F03Role Role { get; set; } = null!;
+
+    [ForeignKey(nameof(IdFunction))]
+    public F03Function Function { get; set; } = null!;
 }
