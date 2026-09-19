162096fab82cb64c9553c1a49a9af4de5f9d7051
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-19 23:48:14 +0700

    refactor(hrm): deploy attendance pipeline as isolated FVN stages

--- SQL/00_Deploy_All.sql
+++ SQL/00_Deploy_All.sql
@@ -63,7 +63,11 @@ IMPORTANT:
 :r 19_WorkCalendar.sql
 :r 20_Reports.sql
 :r 21_Verify_Reports.sql
-:r 22_HrmCompatibleAttendance.sql
+:r 22_00_HrmAttendanceTables.sql
+:r 22_01_InterSectionTime3.sql
+:r 22_02_HrmCompatibleTimeKeepingForStaff.sql
+:r 22_03_CalculateHrmAttendance.sql
+:r 22_04_GetHrmAttendanceCalculation.sql
 :r 23_LeaveBalanceUpgrade.sql
 :r 24_WorkYearUpgrade.sql
 :r 25_RemoveLegacyOTSync.sql
