e4094b625869dad0b63a97dc7f1f586f8c162f2b
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-19 19:21:54 +0700

    refactor: return equipment submit business result

--- Application/Interfaces/Equipment/IEquipmentService.cs
+++ Application/Interfaces/Equipment/IEquipmentService.cs
@@ -1,4 +1,5 @@
 using FVN_REGISTER.Contract.Dtos.Equipment;
+using FVN_REGISTER.Contract.Utils;
 
 namespace FVN_REGISTER.Application.Interfaces.Equipment;
 
@@ -7,10 +8,10 @@ public interface IEquipmentService
     Task<bool> HasModuleAccessAsync(CancellationToken ct = default);
     Task<List<EquipmentApproverDto>> GetApproversAsync(string deptCode, CancellationToken ct = default);
     Task<EquipmentRequestDto> CreateRegistrationDraftAsync(CreateEquipmentRegistrationDto request, CancellationToken ct = default);
-    Task<EquipmentRequestDto> SubmitRegistrationAsync(int requestId, CancellationToken ct = default);
+    Task<ServiceResult<EquipmentRequestDto>> SubmitRegistrationAsync(int requestId, CancellationToken ct = default);
     Task<List<EquipmentRequestDto>> GetMineAsync(CancellationToken ct = default);
     Task<EquipmentRequestDto> CreateRepairDraftAsync(CreateEquipmentRepairDto request, CancellationToken ct = default);
-    Task<EquipmentRequestDto> SubmitRepairAsync(int requestId, CancellationToken ct = default);
+    Task<ServiceResult<EquipmentRequestDto>> SubmitRepairAsync(int requestId, CancellationToken ct = default);
     Task<EquipmentAssetDto> ScanAsync(string qrToken, CancellationToken ct = default);
     Task<EquipmentAssetDto?> GetAssetAsync(int assetId, CancellationToken ct = default);
 }
