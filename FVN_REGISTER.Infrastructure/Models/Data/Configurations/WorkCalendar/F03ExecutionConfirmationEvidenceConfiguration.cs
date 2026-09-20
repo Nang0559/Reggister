8ac178a075d8916dadd9526328372d5189b90626
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-21 00:23:40 +0700

    fix(execution): map BaseAudit LastModifiedSource explicitly

--- FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionConfirmationEvidenceConfiguration.cs
+++ FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionConfirmationEvidenceConfiguration.cs
@@ -8,6 +8,7 @@ public sealed class F03ExecutionConfirmationEvidenceConfiguration : IEntityTypeC
     public void Configure(EntityTypeBuilder<F03ExecutionConfirmationEvidence> b)
     {
         b.ToTable("F03ExecutionConfirmationEvidence"); b.HasKey(x=>x.Id);
+        b.Property(x => x.LastModifiedSource).HasMaxLength(50);
         b.Property(x=>x.EvidenceType).HasMaxLength(50).IsRequired(); b.Property(x=>x.ReferenceNo).HasMaxLength(200);
         b.Property(x=>x.ExternalUrl).HasMaxLength(1000); b.Property(x=>x.Description).HasMaxLength(2000);
         b.Property(x=>x.ReviewStatus).HasMaxLength(50).IsRequired(); b.Property(x=>x.ReviewNote).HasMaxLength(2000);
