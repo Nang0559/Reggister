0d1b320895bc1ba5a82dfa65273f76d397f2909c
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-20 16:39:55 +0700

    fix: align sync review flag SQL table name

--- SQL/18_ApproverConfigurationReview.sql
+++ SQL/18_ApproverConfigurationReview.sql
@@ -1,17 +1,17 @@
 /*
   HRM -> Approver review. HRM never overwrites F03Approvers.
 */
-IF COL_LENGTH('dbo.F03SyncReviewFlags','CurrentApproverId') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD CurrentApproverId INT NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','OldDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD OldDeptCode NVARCHAR(20) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','OldPositionCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD OldPositionCode NVARCHAR(20) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','NewDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD NewDeptCode NVARCHAR(20) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','NewPositionCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD NewPositionCode NVARCHAR(20) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','CurrentApproverCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD CurrentApproverCode NVARCHAR(50) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','CurrentLevel') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD CurrentLevel INT NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','CurrentRoleName') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD CurrentRoleName NVARCHAR(50) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','CurrentApproveForDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD CurrentApproveForDeptCode NVARCHAR(20) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','SuggestedApproverCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD SuggestedApproverCode NVARCHAR(50) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','SuggestedLevel') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD SuggestedLevel INT NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','SuggestedRoleName') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD SuggestedRoleName NVARCHAR(50) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','SuggestedApproveForDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD SuggestedApproveForDeptCode NVARCHAR(20) NULL;
-IF COL_LENGTH('dbo.F03SyncReviewFlags','Decision') IS NULL ALTER TABLE dbo.F03SyncReviewFlags ADD Decision NVARCHAR(50) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','CurrentApproverId') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD CurrentApproverId INT NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','OldDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD OldDeptCode NVARCHAR(20) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','OldPositionCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD OldPositionCode NVARCHAR(20) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','NewDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD NewDeptCode NVARCHAR(20) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','NewPositionCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD NewPositionCode NVARCHAR(20) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','CurrentApproverCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD CurrentApproverCode NVARCHAR(50) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','CurrentLevel') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD CurrentLevel INT NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','CurrentRoleName') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD CurrentRoleName NVARCHAR(50) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','CurrentApproveForDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD CurrentApproveForDeptCode NVARCHAR(20) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','SuggestedApproverCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD SuggestedApproverCode NVARCHAR(50) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','SuggestedLevel') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD SuggestedLevel INT NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','SuggestedRoleName') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD SuggestedRoleName NVARCHAR(50) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','SuggestedApproveForDeptCode') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD SuggestedApproveForDeptCode NVARCHAR(20) NULL;
+IF COL_LENGTH('dbo.F03SyncReviewFlag','Decision') IS NULL ALTER TABLE dbo.F03SyncReviewFlag ADD Decision NVARCHAR(50) NULL;
