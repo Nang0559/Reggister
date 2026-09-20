bea85ad0fb941f71e839a355d8973194015c9d00
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-20 23:08:31 +0700

    Align EF configuration with canonical SQL source participant keys

--- FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionConfirmationConfiguration.cs
+++ FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionConfirmationConfiguration.cs
@@ -8,7 +8,7 @@ public sealed class F03ExecutionConfirmationConfiguration : IEntityTypeConfigura
     public void Configure(EntityTypeBuilder<F03ExecutionConfirmation> b)
     {
         b.ToTable("F03ExecutionConfirmations"); b.HasKey(x=>x.Id);
-        b.Property(x=>x.ModuleCode).HasMaxLength(50).IsRequired(); b.Property(x=>x.SourceId).HasMaxLength(100).IsRequired();
+        b.Property(x=>x.ModuleCode).HasMaxLength(50).IsRequired(); b.Property(x=>x.SourceType).HasMaxLength(50).IsRequired(); b.Property(x=>x.ParticipantId).HasMaxLength(100); b.Property(x=>x.SourceId).HasMaxLength(100).IsRequired();
         b.Property(x=>x.Decision).HasMaxLength(50); b.Property(x=>x.Status).HasMaxLength(50).IsRequired();
         b.Property(x=>x.Comment).HasMaxLength(2000); b.Property(x=>x.ReviewNote).HasMaxLength(2000);
         b.HasIndex(x=>x.ReconciliationId).IsUnique(); b.HasIndex(x=>new{x.EmployeeId,x.Status,x.WorkDate});
