8f3f97187ccbd8c4fb63aaeb5dc610902bee900a
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-21 02:27:24 +0700

    refactor: remove duplicate SQL prefix 02_Schemas.sql

--- SQL/02_Schemas.sql
+++
@@ -1,8 +0,0 @@
-/*
-EF Core maps the current FVN_REGISTER model to the dbo schema.
-Domain schemas such as auth/leave/ot are intentionally not used.
-*/
-USE [FVN_REGISTER];
-GO
-IF SCHEMA_ID(N'dbo') IS NULL EXEC(N'CREATE SCHEMA [dbo]');
-GO
