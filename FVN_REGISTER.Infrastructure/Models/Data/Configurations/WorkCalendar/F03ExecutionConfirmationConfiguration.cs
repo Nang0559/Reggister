316365c4de14bd407bf039ad70c72e05541ae8a7
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-20 22:34:16 +0700

    feat: map shared execution reconciliation entities

---
+++ FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionConfirmationConfiguration.cs
@@ -0,0 +1,16 @@
+using FVN_REGISTER.Core.Entities.WorkCalendar;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FVN_REGISTER.Infrastructure.Models.Data.Configurations.WorkCalendar;
+public sealed class F03ExecutionConfirmationConfiguration : IEntityTypeConfiguration<F03ExecutionConfirmation>
+{
+    public void Configure(EntityTypeBuilder<F03ExecutionConfirmation> b)
+    {
+        b.ToTable("F03ExecutionConfirmations"); b.HasKey(x=>x.Id);
+        b.Property(x=>x.ModuleCode).HasMaxLength(50).IsRequired(); b.Property(x=>x.SourceId).HasMaxLength(100).IsRequired();
+        b.Property(x=>x.Decision).HasMaxLength(50); b.Property(x=>x.Status).HasMaxLength(50).IsRequired();
+        b.Property(x=>x.Comment).HasMaxLength(2000); b.Property(x=>x.ReviewNote).HasMaxLength(2000);
+        b.HasIndex(x=>x.ReconciliationId).IsUnique(); b.HasIndex(x=>new{x.EmployeeId,x.Status,x.WorkDate});
+    }
+}
