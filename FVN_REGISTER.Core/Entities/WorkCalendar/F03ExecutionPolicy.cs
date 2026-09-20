7eaad352d64e91889d738392ffb514af934168de
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-20 22:34:20 +0700

    feat: map shared execution reconciliation entities

---
+++ FVN_REGISTER.Core/Entities/WorkCalendar/F03ExecutionPolicy.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FVN_REGISTER.Core.Entities.WorkCalendar;
+[Table("F03ExecutionPolicies")]
+public sealed class F03ExecutionPolicy : BaseAuditEntity
+{
+    public int Id { get; set; }
+    [Required, StringLength(50)] public string ModuleCode { get; set; } = string.Empty;
+    public byte ReconciliationMode { get; set; } = 1;
+    public byte ConfirmationMode { get; set; }
+    public byte EvidenceMode { get; set; } = 2;
+    public byte ReviewMode { get; set; } = 1;
+    public int? DueHours { get; set; }
+    public byte AutoResolveMode { get; set; }
+}
