e36fa6e0500bd4cb2e48c04ca9fd415349d44d3d
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-20 16:00:12 +0700

    refactor(phase1): remove unused service interface methods

--- Application/Interfaces/Emails/IEmailService.cs
+++ Application/Interfaces/Emails/IEmailService.cs
@@ -5,35 +5,14 @@ namespace FVN_REGISTER.Application.Interfaces.Emails
 {
     public interface IEmailService
     {
-        Task<EmailQueueResult> QueueEmail(
-            string to,
-            string templateCode,
-            object data,
-            CancellationToken ct = default);
-
+        Task<EmailQueueResult> QueueEmail(string to, string templateCode, object data, CancellationToken ct = default);
         Task ProcessQueue(CancellationToken ct = default);
-
-        Task SendEmail(
-            string to,
-            string subject,
-            string body,
-            CancellationToken ct = default);
-
+        Task SendEmail(string to, string subject, string body, CancellationToken ct = default);
         string RenderTemplate(string template, string jsonPayload);
-
-        Task RetryFailedEmails(CancellationToken ct = default);
-        Task<int> GetPendingCount(CancellationToken ct = default);
-        Task<int> GetFailedCount(CancellationToken ct = default);
-        Task<List<EmailQueueDto>> GetFailedEmails(CancellationToken ct = default);
         Task<List<EmailQueueDto>> GetQueueAsync(int take = 500, CancellationToken ct = default);
         Task RetryEmailBatchAsync(IReadOnlyCollection<int> ids, CancellationToken ct = default);
         Task CancelEmailBatchAsync(IReadOnlyCollection<int> ids, CancellationToken ct = default);
         Task CancelEmail(int id, CancellationToken ct = default);
         Task ResendEmail(int id, CancellationToken ct = default);
-
-        Task SendApprovalRequestAsync(
-            LeaveApprovalEmailDto data,
-            string toEmail,
-            CancellationToken ct = default);
     }
 }
