c669019ee6d9341267892bb1d430f1b64f7023fd
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-21 00:23:33 +0700

    fix(execution): map BaseAudit LastModifiedSource explicitly

--- FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionReconciliationConfiguration.cs
+++ FVN_REGISTER.Infrastructure/Models/Data/Configurations/WorkCalendar/F03ExecutionReconciliationConfiguration.cs
@@ -8,6 +8,7 @@ public sealed class F03ExecutionReconciliationConfiguration : IEntityTypeConfigu
     public void Configure(EntityTypeBuilder<F03ExecutionReconciliation> b)
     {
         b.ToTable("F03ExecutionReconciliations"); b.HasKey(x=>x.Id);
+        b.Property(x => x.LastModifiedSource).HasMaxLength(50);
         b.Property(x=>x.ModuleCode).HasMaxLength(50).IsRequired(); b.Property(x=>x.SourceType).HasMaxLength(50).IsRequired(); b.Property(x=>x.ParticipantId).HasMaxLength(100);
         b.Property(x=>x.SourceId).HasMaxLength(100).IsRequired();
         b.Property(x=>x.PlannedState).HasMaxLength(50); b.Property(x=>x.ActualState).HasMaxLength(50);
