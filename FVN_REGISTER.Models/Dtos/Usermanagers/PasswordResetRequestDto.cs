efaea77bdc2d9fdb6d8e13cd8311575bc06485e5
Nang Do Van <pc10@fcc-vn.com.vn>
2026-09-21 13:57:00 +0700

    ok

---
+++ FVN_REGISTER.Models/Dtos/Usermanagers/PasswordResetRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FVN_REGISTER.Contract.Dtos.Usermanagers
+{
+    internal class PasswordResetRequestDto
+    {
+    }
+}
---
+++ FVN_REGISTER.Models/Requests/Users/PasswordResetRequestCreateDto.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FVN_REGISTER.Contract.Requests.Users
+{
+    public sealed class PasswordResetRequestCreateDto
+    {
+        [Required(ErrorMessage = "Mã nhân viên không được để trống")]
+        [StringLength(50)]
+        public string EmployeeCode { get; set; } = string.Empty;
+
+        [StringLength(1000)]
+        public string? RequestNote { get; set; }
+    }
+}
