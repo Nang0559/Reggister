b4410e0814664598417fe1256ea4f46c2f19ec43
Nang Do Van <pc10@fcc-vn.com.vn>
2026-09-21 17:03:23 +0700

    ok

--- Application/Interfaces/PublicInformation/IPublicInformationService.cs
+++ Application/Interfaces/PublicInformation/IPublicInformationService.cs
@@ -1,5 +1,6 @@
 
 using FVN_REGISTER.Contract.Dtos.PublicInformation;
+using FVN_REGISTER.Contract.Requests.PublicInformation;
 using FVN_REGISTER.Contract.Utils;
 
 namespace FVN_REGISTER.Application.Interfaces.PublicInformation;
