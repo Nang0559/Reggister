bc215f0dd0d46334e4e752cf0c71e36c205186f5
Nang Do Van <pc10@fcc-vn.com.vn>
2026-09-21 14:23:35 +0700

    ok

---
+++ FVN_REGISTER.Models/Requests/Users/PasswordResetRequestProcessDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FVN_REGISTER.Contract.Requests.Users;
+
+public sealed class PasswordResetRequestProcessDto
+{
+    public bool Approve { get; set; } = true;
+
+    [StringLength(1000)]
+    public string? ResultNote { get; set; }
+}
