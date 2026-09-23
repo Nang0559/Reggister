082e62c90cbb10e3ef0ae8897d10eb6e888d3315
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-23 07:52:59 +0700

    fix(security): add HR IT permission codes and retire approver role checks

--- FVN_REGISTER.Core/Constants/UserPermissionCodes.cs
+++ FVN_REGISTER.Core/Constants/UserPermissionCodes.cs
@@ -9,6 +9,8 @@ namespace FVN_REGISTER.Core.Constants
         public const int Approver = 4;
         public const int User = 5;
         public const int Guest = 6;
+        public const int HR = 7;
+        public const int IT = 8;
     }
 
     public static class UserFunctionCodes
@@ -22,12 +24,15 @@ namespace FVN_REGISTER.Core.Constants
         public static bool IsSuperAdmin(this int? code) => code == UserPermissionCodes.SuperAdmin;
         public static bool IsAdmin(this int? code) => code == UserPermissionCodes.SuperAdmin || code == UserPermissionCodes.Admin;
         public static bool IsEditor(this int? code) => code == UserPermissionCodes.SuperAdmin || code == UserPermissionCodes.Admin || code == UserPermissionCodes.Editor;
+        [Obsolete("Approval is resolved by capability + F03ApprovalPolicies. Do not use role to decide approval.")]
         public static bool IsApprover(this int? code) => code == UserPermissionCodes.SuperAdmin || code == UserPermissionCodes.Admin || code == UserPermissionCodes.Editor || code == UserPermissionCodes.Approver;
         public static bool IsUser(this int? code) => code == UserPermissionCodes.User;
         public static bool IsGuest(this int? code) => code == UserPermissionCodes.Guest;
+        public static bool IsHr(this int? code) => code == UserPermissionCodes.HR;
+        public static bool IsIt(this int? code) => code == UserPermissionCodes.IT;
         public static bool IsNormalUser(this int? code) => code == UserPermissionCodes.User || code == UserPermissionCodes.Guest;
-        public static bool CanRequestLeave(this int? code) => code == UserPermissionCodes.Approver || code == UserPermissionCodes.User;
-        public static bool CanViewReport(this int? code) => code >= 1 && code <= 4;
+        public static bool CanRequestLeave(this int? code) => code == UserPermissionCodes.Approver || code == UserPermissionCodes.User || code == UserPermissionCodes.HR || code == UserPermissionCodes.IT;
+        public static bool CanViewReport(this int? code) => (code >= 1 && code <= 4) || code == UserPermissionCodes.HR || code == UserPermissionCodes.IT;
         public static bool CanManageData(this int? code) => code == UserPermissionCodes.SuperAdmin || code == UserPermissionCodes.Admin || code == UserPermissionCodes.Editor;
         public static bool IsSuperAdmin(this int code) => ((int?)code).IsSuperAdmin();
         public static bool IsAdmin(this int code) => ((int?)code).IsAdmin();
@@ -35,6 +40,8 @@ namespace FVN_REGISTER.Core.Constants
         public static bool IsApprover(this int code) => ((int?)code).IsApprover();
         public static bool IsUser(this int code) => ((int?)code).IsUser();
         public static bool IsGuest(this int code) => ((int?)code).IsGuest();
+        public static bool IsHr(this int code) => ((int?)code).IsHr();
+        public static bool IsIt(this int code) => ((int?)code).IsIt();
         public static bool IsNormalUser(this int code) => ((int?)code).IsNormalUser();
         public static bool CanRequestLeave(this int code) => ((int?)code).CanRequestLeave();
         public static bool CanViewReport(this int code) => ((int?)code).CanViewReport();
