6b9d75c079198c91d76999c07dbfd6d9498c6979
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-24 12:38:27 +0700

    refactor(equipment): align equipment service interface with ServiceResult

--- Application/Interfaces/Equipment/IEquipmentService.cs
+++ Application/Interfaces/Equipment/IEquipmentService.cs
@@ -5,13 +5,13 @@ namespace FVN_REGISTER.Application.Interfaces.Equipment;
 
 public interface IEquipmentService
 {
-    Task<bool> HasModuleAccessAsync(CancellationToken ct = default);
-    Task<List<EquipmentApproverDto>> GetApproversAsync(string deptCode, CancellationToken ct = default);
-    Task<EquipmentRequestDto> CreateRegistrationDraftAsync(CreateEquipmentRegistrationDto request, CancellationToken ct = default);
+    Task<ServiceResult<bool>> HasModuleAccessAsync(CancellationToken ct = default);
+    Task<ServiceResult<List<EquipmentApproverDto>>> GetApproversAsync(string deptCode, CancellationToken ct = default);
+    Task<ServiceResult<EquipmentRequestDto>> CreateRegistrationDraftAsync(CreateEquipmentRegistrationDto request, CancellationToken ct = default);
     Task<ServiceResult<EquipmentRequestDto>> SubmitRegistrationAsync(int requestId, CancellationToken ct = default);
-    Task<List<EquipmentRequestDto>> GetMineAsync(CancellationToken ct = default);
-    Task<EquipmentRequestDto> CreateRepairDraftAsync(CreateEquipmentRepairDto request, CancellationToken ct = default);
+    Task<ServiceResult<List<EquipmentRequestDto>>> GetMineAsync(CancellationToken ct = default);
+    Task<ServiceResult<EquipmentRequestDto>> CreateRepairDraftAsync(CreateEquipmentRepairDto request, CancellationToken ct = default);
     Task<ServiceResult<EquipmentRequestDto>> SubmitRepairAsync(int requestId, CancellationToken ct = default);
-    Task<EquipmentAssetDto> ScanAsync(string qrToken, CancellationToken ct = default);
-    Task<EquipmentAssetDto?> GetAssetAsync(int assetId, CancellationToken ct = default);
+    Task<ServiceResult<EquipmentAssetDto>> ScanAsync(string qrToken, CancellationToken ct = default);
+    Task<ServiceResult<EquipmentAssetDto?>> GetAssetAsync(int assetId, CancellationToken ct = default);
 }
