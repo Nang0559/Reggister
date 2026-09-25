4d56dd0b60ed767e04ccab96231f0c46d5939af8
Nang Do Van <pc10@fcc-vn.com.vn>
2026-09-25 17:10:39 +0700

    ok

--- Application/Interfaces/PublicForms/IPublicFormService.cs
+++ Application/Interfaces/PublicForms/IPublicFormService.cs
@@ -7,15 +7,76 @@ namespace FVN_REGISTER.Application.Interfaces.PublicForms;
 public interface IPublicFormService
 {
     Task<List<PublicFormDto>> GetManageListAsync(CancellationToken ct = default);
-    Task<List<PublicFormDto>> GetAvailableAsync(string employeeCode, string? deptCode, string? positionCode, CancellationToken ct = default);
-    Task<PublicFormDto?> GetAsync(int id, CancellationToken ct = default);
-    Task<ServiceResult<PublicFormDto>> CreateAsync(SavePublicFormRequest request, int actorUserId, CancellationToken ct = default);
-    Task<ServiceResult<PublicFormDto>> UpdateAsync(int id, SavePublicFormRequest request, int actorUserId, CancellationToken ct = default);
-    Task<ServiceResult> PublishAsync(int id, int actorUserId, CancellationToken ct = default);
-    Task<ServiceResult> CloseAsync(int id, int actorUserId, CancellationToken ct = default);
-    Task<ServiceResult<int>> SubmitAsync(int formId, string employeeCode, string? deptCode, string? positionCode, IReadOnlyCollection<PublicFormAnswerRequest> answers, CancellationToken ct = default);
-    Task<List<PublicFormDto>> GetSubmissionFormsAsync(CancellationToken ct = default);
-    Task<ServiceResult<PublicFormSubmissionListDto>> GetSubmissionsAsync(int formId, PublicFormSubmissionQueryDto query, string scopeCode, CancellationToken ct = default);
-    Task<ServiceResult<PublicFormSubmissionSummaryDto>> GetSubmissionSummaryAsync(int formId, PublicFormSubmissionQueryDto query, string scopeCode, CancellationToken ct = default);
-    Task<ServiceResult<byte[]>> ExportSubmissionsAsync(int formId, PublicFormSubmissionQueryDto query, string scopeCode, CancellationToken ct = default);
+
+    Task<List<PublicFormDto>> GetAvailableAsync(
+        string employeeCode,
+        string? deptCode,
+        string? positionCode,
+        CancellationToken ct = default);
+
+    Task<PublicFormDto?> GetAsync(
+        int id,
+        CancellationToken ct = default);
+
+    Task<List<PublicFormAudienceLookupDto>> GetAudienceDepartmentsAsync(
+        CancellationToken ct = default);
+
+    Task<List<PublicFormAudienceLookupDto>> GetAudiencePositionsAsync(
+        CancellationToken ct = default);
+
+    Task<PublicFormAudienceEmployeePageDto> SearchAudienceEmployeesAsync(
+        string? search,
+        int page,
+        int pageSize,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<PublicFormDto>> CreateAsync(
+        SavePublicFormRequest request,
+        int actorUserId,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<PublicFormDto>> UpdateAsync(
+        int id,
+        SavePublicFormRequest request,
+        int actorUserId,
+        CancellationToken ct = default);
+
+    Task<ServiceResult> PublishAsync(
+        int id,
+        int actorUserId,
+        CancellationToken ct = default);
+
+    Task<ServiceResult> CloseAsync(
+        int id,
+        int actorUserId,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<int>> SubmitAsync(
+        int formId,
+        string employeeCode,
+        string? deptCode,
+        string? positionCode,
+        IReadOnlyCollection<PublicFormAnswerRequest> answers,
+        CancellationToken ct = default);
+
+    Task<List<PublicFormDto>> GetSubmissionFormsAsync(
+        CancellationToken ct = default);
+
+    Task<ServiceResult<PublicFormSubmissionListDto>> GetSubmissionsAsync(
+        int formId,
+        PublicFormSubmissionQueryDto query,
+        string scopeCode,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<PublicFormSubmissionSummaryDto>> GetSubmissionSummaryAsync(
+        int formId,
+        PublicFormSubmissionQueryDto query,
+        string scopeCode,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<byte[]>> ExportSubmissionsAsync(
+        int formId,
+        PublicFormSubmissionQueryDto query,
+        string scopeCode,
+        CancellationToken ct = default);
 }
